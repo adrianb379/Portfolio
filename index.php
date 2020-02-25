c3ec06e2a089315cb3cf4acc5a2ccc339b6bf957
adrian <adrianbogado67@gmail.com>
2020-02-25 13:26:45 -0300

    Archivo index

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Adrian Bogado| Desarrollador</title>
+        <link rel="shortcut icon" href="img/icon.png" type="image/x-icon">
+        <link rel="stylesheet" href="css/index.css">
+    </head>
+    <body>
+
+        <header>
+            <h1>Adrian Bogado</h1>
+            <p>Desarrollador</p>
+        </header>
+
+        <section id="projects">
+            <div class="inProjects">
+                <a href="apps/ContactList"><button>Lista de Contactos</button></a>
+                <a href="apps/cash-machine"><button>Cajero Automatico</button></a>
+            </div>
+        </section>
+
+        <script src="index.js"></script>
+    </body>
+</html>
