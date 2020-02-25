1fc7f386de65488d1fd3ded7bd941ce712421904
adrian <adrianbogado67@gmail.com>
2020-02-25 13:51:59 -0300

    cambios

--- apps/ContactList/app/views/newcontact.php
+++ apps/ContactList/app/views/newcontact.php
@@ -15,23 +15,23 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Novo contato</title>
+        <title>Nuevo contacto</title>
         <link rel="stylesheet" href="<?php echo DIRPAGE.'/public/css/newcontact.css' ?>">
     </head>
     <body>
 
         <header>
-            <p>Novo contato</p>
+            <p>Nuevo contacto</p>
         </header>
 
         <section>
             <div>
                 <form action="newitem" method="post">
-                    <input type="text" name="namee" placeholder="Nome" required>
-                    <input type="tel" name="phone" placeholder="Telefone" required>
+                    <input type="text" name="namee" placeholder="Name" required>
+                    <input type="tel" name="phone" placeholder="Telephone" required>
                     <input type="submit" value="Incluir">
                 </form>
-                <a href="<?php echo DIRPAGE; ?>"><p><< VOLTAR</p></a>
+                <a href="<?php echo DIRPAGE; ?>"><p><< VOLVER</p></a>
             </div>
         </section>
 
--- apps/ContactList/app/views/signup.php
+++ apps/ContactList/app/views/signup.php
@@ -4,24 +4,24 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Cadastro</title>
+        <title>Registrarse</title>
         <link rel="stylesheet" href="<?php echo DIRPAGE.'/public/css/signup.css' ?>">
     </head>
     <body>
 
         <header>
-            <p>Cadastro</p>
+            <p>Registro</p>
         </header>
 
         <section>
             <div>
                 <form action="signingup" method="post">
-                    <input type="text" name="namee" placeholder="Nome" required>
-                    <input type="email" name="email" placeholder="E-mail" required>
-                    <input type="password" name="pass" placeholder="Senha" required>
+                    <input type="text" name="namee" placeholder="Name" required>
+                    <input type="email" name="email" placeholder="Email" required>
+                    <input type="password" name="pass" placeholder="Password" required>
                     <input type="submit" value="Cadastrar">
                 </form>
-                <p>Já possui cadastro? <a href="login">Clique aqui</a></p>
+                <p>Ya esta registrado? <a href="login">Haga click aqui</a></p>
             </div>
         </section>
 
