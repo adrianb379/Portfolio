9e49d8276d56d44897b36277d8667f1e6665dbc1
adrian <adrianbogado67@gmail.com>
2020-02-27 13:19:32 -0300

    algunos cambios

---
+++ contacto.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ariel Jans | Diseño y Programación Web</title>
+    <link href="https://fonts.googleapis.com/css?family=BenchNine|Cabin+Condensed|Fjalla+One|Michroma|Montserrat|Teko|Varela&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/index.css">
+    <!-- Place your kit's code here -->
+    <script src="https://kit.fontawesome.com/414006261c.js" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+  <div class="container">
+    <header>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+<a class="navbar-brand" href="#"><img  src="img/icon.png" class="img-fluid" ></a>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+<span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+<div class="navbar-nav">
+  <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
+  <a class="nav-item nav-link" href="contacto.php">Sobre Mi</a>
+
+</div>
+</div>
+</nav>
+    </header>
+
+    <main class="container-gral">
+        <div class="flex-container">
+            <div class="contenido-img">
+                <img src="img/perfil.png" alt="">
+            </div>
+            <div class="contenido-tex">
+                <h1> Sobre Mi</h1>
+                <p>Hola! Me llamo Adrian Bogado, tengo 23 años.  Soy estudiante de la carrera de Analista de Sistemas (Actualmente en 2do año) y Soy egresado del curso Full Stack Web Developer en Digial House. Me gusta programar y todo el tiempo aprender cosas nuevas. </p>
+            </div>
+        </div>
+
+
+
+        <div class="contacto">
+            <h1> Contacto</h1>
+        <a href="https://www.linkedin.com/in/adrian-bogado-7747aaab/" target="blank"><i class="fab fa-linkedin icono-linkedin redes-hover"></i></a>
+        <a href="https://github.com/adrianb379" target="blank"><i class="fab fa-github-square icono-github redes-hover"></i></a>
+        <a href="mailto:adrianbogado67@gmail.com" target="blank"><i class="fas fa-envelope-square icono-email redes-hover"></i></a>
+        <a href="https://api.whatsapp.com/send?phone=541130589941&text=&source=&data=" target="blank"><i class="fab fa-whatsapp-square icono-whatsapp redes-hover"></i></a>
+        </div>
+
+    </main>
+</div>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -13,23 +13,22 @@
       <div class="container">
         <header>
           <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="#"><img  src="img/icon.png" class="img-fluid" ></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
       <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">Features</a>
-      <a class="nav-item nav-link" href="#">Pricing</a>
-      <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      <a class="nav-item nav-link" href="contacto.php">Sobre Mi</a>
+
     </div>
   </div>
 </nav>
 
 
             <h1>Adrian Bogado</h1>
-            <p>Desarrollador</p>
+            <p>Desarrollador Junior</p>
         </header>
 
         <section id="projects">
@@ -38,6 +37,36 @@
                 <a href="apps/cash-machine"><button>Cajero Automatico</button></a>
             </div>
         </section>
+        <div class="user-details">
+  <p class="h1">Principales Proyectos</p>
+</div>
+<div class="user-projects">
+  <div class="images-right">
+    <img  src="img/ecom.jpg" class="img-fluid" alt="Responsive image">
+  </div>
+  <div class="contents">
+    <h2> Ecommerce </h2>
+    <ul>
+      <li> Caracteristicas</li>
+    </ul>
+    <p>Ecommerce desarrollado con HTML,CSS,PHP Y MYSQL. Cuenta con un abm de productos, registro,login, contacto,faqs,entre otras.</p>
+    <a class="project-link" href="#">Ir al repositorio</a>
+  </div>
+</div>
+<div class="user-projects">
+  <div class="images-left">
+    <img  src="img/game.jpg" class="img-fluid" alt="Responsive image">
+  </div>
+  <div class="contents-right">
+    <h4> Juego de Preguntas y Respuestas </h4>
+    <ul>
+      <li> Caracteristicas</li>
+    </ul>
+    <p> Juego de preguntas y respuestas desarrollado con HTML,CSS,PHP,POO,MY SQL. Luego se paso el proyecto a LARAVEL
+    El mismo cuenta con un panel de admin(carga,alta,baja de preguntas), registros,login,faqs,contactos,entre otros</p>
+    <a class="project-link" href="#">Ir al repositorio</a>
+  </div>
+</div>
 
         <script src="index.js"></script>
         </div>
